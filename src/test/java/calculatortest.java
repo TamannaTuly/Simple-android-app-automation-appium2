774d23cc8e27d229795b1dec82b151f502922097
TamannaTuly <ttniger53@gmail.com>
2022-02-13 15:39:28 +0600

    Initial commit

---
+++ src/test/java/calculatortest.java
@@ -0,0 +1,39 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+//import org.asynchttpclient.uri.Uri;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class calculatortest {
+
+//    WebDriver webDriver;
+    static AppiumDriver<MobileElement> appiumDriver;
+//    AndroidDriver androidDriver;
+
+    public static void main(String[] args) {
+
+        try {
+            openCalclutaor();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void openCalclutaor() throws MalformedURLException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("deviceName","Galaxy A30");
+        cap.setCapability("udid","R58M829Q3WD");
+        cap.setCapability("platformName","Android");
+        cap.setCapability("platformVersion","10");
+        cap.setCapability("appPackage","com.sec.android.app.popupcalculator");
+        cap.setCapability("appActivity","com.sec.android.app.popupcalculator.Calculator");
+
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+        appiumDriver = new AppiumDriver<MobileElement>(url,cap);
+        System.out.println("app started .....");
+    }
+}
